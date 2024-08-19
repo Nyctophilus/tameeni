b4d8d82dc10e7b83f19e9ce400f9c59fdc2357a2
Nyctophilus <mohammed.yuossry@gmail.com>
2024-08-20 01:04:27 +0300

    banner to note it's not in actiive use

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,24 +1,6 @@
 import { X } from "lucide-react";
 import { useState } from "react";
 
-const CloseIcon = ({ className, ...props }: { className?: string }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className={className}
-    {...props}
-  >
-    <path d="M18 6 6 18" />
-    <path d="m6 6 12 12" />
-  </svg>
-);
 
 export default function CollapsibleBanner() {
   const [isVisible, setIsVisible] = useState(true);
